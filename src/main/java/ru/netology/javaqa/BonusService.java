e674aeaf9420abdca10c5d1df46893180b50bc9a
Igor Kapitonov <kizero182@gmail.com>
2024-02-12 21:09:42 +0300

    Initial commit

---
+++ src/main/java/ru/netology/javaqa/BonusService.java
@@ -0,0 +1,17 @@
+package ru.netology.javaqa;
+
+public class BonusService {
+    public long calculate(boolean registered) {
+        return calculate(034L, registered);
+    }
+
+    public long calculate(long amount, boolean registered) {
+        int percent = registered ? 3 : 1;
+        long bonus = amount * percent / 100;
+        long limit = 500;
+        if (bonus > limit) {
+            bonus = limit;
+        }
+        return bonus;
+    }
+}
